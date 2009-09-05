55ae01de855947212ce74ac2fba8b0e75bf21ca4
mollifier <mollifier@gmail.com>
2009-09-05 12:21:28 +0900

    Remove (function() { ... })();

--- disableAnchor.js
+++ disableAnchor.js
@@ -1,16 +1,14 @@
-(function() {
-  jetpack.statusBar.append({
-    html: "disA",
-    width: 60,
-    onReady: function(widget) {
-      $(widget).click(function() {
-        $(jetpack.tabs.focused.contentDocument).
-          find("a").each(function() {
-            $(this).replaceWith($(this).html());
-          });
+jetpack.statusBar.append({
+  html: "disA",
+  width: 60,
+  onReady: function(widget) {
+    $(widget).click(function() {
+      $(jetpack.tabs.focused.contentDocument).
+      find("a").each(function() {
+        $(this).replaceWith($(this).html());
       });
-    }
-  });
-})();
+    });
+  }
+});
 
 // vim:set ts=2 sw=2:
